f2ad8d1375beb3ace2e3eb16138e3fb263ec9162
Mitsushiro Hirano <mitsushiro1@gmail.com>
2023-03-21 23:45:37 -0400

    added files

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put(content);
+  await tx.complete;
+  console.log('Content added to database:', content);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const result = await store.getAll();
+  await tx.complete;
+  console.log('Content retrieved from database:', result);
+  return result;
+};
+
 
 initdb();
